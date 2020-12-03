8dc6f40c0cefe22d95f080a34ef7b5f7328d654f
frasnym <frastyawan.nym@gmail.com>
2020-12-03 13:13:33 +0800

    Nodemon on DEV

--- src/index.server.js
+++ src/index.server.js
@@ -4,6 +4,10 @@ const app = express();
 
 env.config();
 
+app.get("/", (req, res) => {
+	res.status(200);
+});
+
 app.listen(process.env.PORT, () => {
 	console.log("Server run on port:", process.env.PORT);
 });
