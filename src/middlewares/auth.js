9a7593ed3c406921d52b8c299b80ee61f2eeabfd
frasnym <frastyawan.nym@gmail.com>
2020-12-04 12:02:48 +0800

    Code Refactor

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -36,33 +36,9 @@ const authCheckUser = async (req, res, next) => {
 
 const authCheckAdmin = async (req, res, next) => {
 	try {
-		if (!req.header("Authorization")) {
-			throw new Error("PleaseAuthenticate");
-		}
-
-		const Authorization = req.header("Authorization").split(" ");
-		const token = Authorization[1];
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-		const user = await UserModel.findOne({
-			_id: decoded._id,
-			"tokens.token": token,
-		});
-
-		if (!user) {
-			throw new Error("PleaseAuthenticate");
-		}
-
-		if (user.account_status != "ACTIVE") {
-			throw new Error("AccountInactive");
-		}
-
-		if (user.role != "ADMIN") {
+		if (req.user.role != "ADMIN") {
 			throw new Error("AccessForbidden");
 		}
-
-		req.token = token;
-		req.user = user;
 		next();
 	} catch (e) {
 		res.respMessage.message = req.t(e.message);
--- src/routers/categoryRouter.js
+++ src/routers/categoryRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
-const { authCheckAdmin } = require("../middlewares/auth");
+const { authCheckAdmin, authCheckUser } = require("../middlewares/auth");
 const categoryController = require("../controllers/categoryController");
 const router = new express.Router();
 
 // router.post("/", categoryController.createCategory);
 router
 	.route("/")
-	.post(authCheckAdmin, categoryController.createCategory)
+	.post(authCheckUser, authCheckAdmin, categoryController.createCategory)
 	.get(categoryController.readCategories);
 
 module.exports = router;
