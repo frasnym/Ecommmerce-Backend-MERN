98ec9c3197d5cbfc707e8c936a617b28576e99fe
frasnym <frastyawan.nym@gmail.com>
2020-12-04 12:20:04 +0800

    JWT Expiry

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -182,7 +182,8 @@ userSchema.methods = {
 		const user = this;
 		const token = jwt.sign(
 			{ _id: user._id.toString() },
-			process.env.JWT_SECRET
+			process.env.JWT_SECRET,
+			{ expiresIn: "1d" } // expired in 1 day
 		);
 
 		const findIndex = user.tokens.findIndex((token_doc) => {
