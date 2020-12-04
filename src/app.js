565ae7453c35ef3644e59707e61c78f1ba185a60
frasnym <frastyawan.nym@gmail.com>
2020-12-04 11:11:53 +0800

    Remove body-parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 require("./db/mongoose");
 
 const { i18next, i18nextMiddleware } = require("./utils/translation");
@@ -8,9 +7,7 @@ const authRouter = require("./routers/authRouter");
 const userRouter = require("./routers/userRouter");
 
 const app = express();
-//! app.use(express.json()); // body raw JSON
-// app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
+app.use(express.json()); // body raw JSON
 app.use(i18nextMiddleware.handle(i18next)); // we tell Express to use i18next's middleware
 app.use("/users/auth", setResponseTemplate, authRouter);
 app.use("/users", setResponseTemplate, userRouter);
