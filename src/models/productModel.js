b722432ccdf6fdff69dca8678276bd453790af3a
frasnym <frastyawan.nym@gmail.com>
2020-12-04 12:54:15 +0800

    Models: Product

---
+++ src/models/productModel.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const mongo_errors = require("../db/mongo_errors");
+
+const productSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "ParameterValueRequired"],
+			trim: true,
+		},
+		slug: {
+			type: String,
+			required: [true, "ParameterValueRequired"],
+			trim: true,
+			unique: true,
+		},
+		price: {
+			type: Number,
+			required: [true, "ParameterValueRequired"],
+		},
+		description: {
+			type: String,
+			required: [true, "ParameterValueRequired"],
+			trim: true,
+		},
+		offer: {
+			type: Number,
+		},
+		images: [
+			{
+				url: {
+					type: String,
+				},
+			},
+		],
+		reviews: [
+			{
+				userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+				review: { type: String },
+			},
+		],
+		category: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Category",
+		},
+		createdBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+productSchema.post("save", function (error, doc, next) {
+	const mongoErr = mongo_errors(error);
+	if (mongoErr) {
+		next(new Error(mongoErr));
+	} else {
+		next();
+	}
+});
+
+module.exports = mongoose.model("Product", productSchema);
